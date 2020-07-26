ffe3377d652ad64020688f20df281a27f1028a86
QASAK <w663012911@outlook.com>
2020-07-26 11:46:21 +0800

    Create mapreduce.py

---
+++ mapreduce/mapreduce.py
@@ -0,0 +1,26 @@
+urls = [url1, url2, ... ]
+# We get all domains here
+domains = map(lambda u: u.split('/')[0], urls)
+
+def get_domain_stat(stat, domain):
+    if domain not in stat:
+        stat[domain] = 0
+    stat[domain] += 1
+    return stat
+
+# We get the stat of domains here
+domain_stat = reduce(get_domain_stat, domains, {})
+
+
+
+# reduce intrucduction:
+functools.reduce(function, iterable[, initializer])
+def reduce(function, iterable, initializer=None):
+    it = iter(iterable)
+    if initializer is None:
+        value = next(it)
+    else:
+        value = initializer
+    for element in it:
+        value = function(value, element)
+    return value
